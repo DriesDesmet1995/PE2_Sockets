f49f33beddeb382fc2934b9cdcad83edeaa2b249
Dries Desmet <61475643+DriesDesmet1995@users.noreply.github.com>
2022-01-03 16:29:41 +0100

    implemented Interface (Server)

--- Ait.FTSock.Server/Ait.FTSock.Server/MainWindow.xaml.cs
+++ Ait.FTSock.Server/Ait.FTSock.Server/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Ait.FTSock.Server.Class.Helpers;
+using Ait.FTSock.Server.Class.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +28,73 @@ namespace Ait.FTSock.Server
         {
             InitializeComponent();
         }
+        #region Global variables
+        DirectoryService directoryService;
+        FileService fileService;
+        Socket serverSocket;
+        IPEndPoint serverEndpoint;
+        bool serverOnline = false;
+        #endregion
+
+        #region eventHandlers
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            directoryService = new DirectoryService();
+            fileService = new FileService();
+            StartupConfig();
+        }
+        private void BtnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStopServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CmbPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CmbIPs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        #endregion
+
+
+        #region methods
+        private void StartupConfig()
+        {
+            cmbIPs.ItemsSource = IPv4Helper.GetActiveIP4s();
+            for (int port = 49200; port <= 49500; port++)
+            {
+                cmbPorts.Items.Add(port);
+            }
+            AppConfig.GetConfig(out string savedIP, out int savedPort);
+            try
+            {
+                cmbIPs.SelectedItem = savedIP;
+            }
+            catch
+            {
+                cmbIPs.SelectedItem = "127.0.0.1";
+            }
+            try
+            {
+                cmbPorts.SelectedItem = savedPort;
+            }
+            catch
+            {
+                cmbPorts.SelectedItem = 49200;
+            }
+            btnStartServer.Visibility = Visibility.Visible;
+            btnStopServer.Visibility = Visibility.Hidden;
+        }
+        #endregion
+
+
     }
 }
